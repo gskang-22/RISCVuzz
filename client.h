1c7bb29313eb0cc870eb50f24dcf318ffa68eb9c
gskang-22 <gskang2002@gmail.com>
2025-07-28 17:07:41 +0800

    starting github from week 2

---
+++ client.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void setup_signal_handlers();
+void initialise();
+uint8_t *allocate_executable_buffer(size_t size);
+void inject_instructions(uint8_t *buf, const uint32_t *instrs, size_t num_instrs);
+void print_reg_changes();
---
+++ server.c
@@ -0,0 +1,6 @@
+/*
+Code for server
+Aim: 
+    1. Generate test cases for the client to run 
+    2. Analyze the results sent back by client, and compare them (if there are multiple boards) 
+*/
