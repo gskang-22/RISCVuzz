f0ff97b6c517fb4229e235c145b15eb913b9f3be
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-09-08 16:39:38 +0800

    fixing error

--- client/sandbox.h
+++ client/sandbox.h
@@ -20,6 +20,5 @@ uint8_t *allocate_executable_buffer();
 void free_executable_buffer(uint8_t *sandbox);
 void prepare_sandbox(uint8_t *sandbox_ptr);
 void inject_instructions(uint8_t *sandbox_ptr, const uint32_t *instrs, size_t num_instrs);
-void print_reg_changes();
 void unmap_vdso_vvar();
 void compare_reg_changes(uint64_t regs_before[32], uint64_t regs_after[32]);
