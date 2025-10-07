3a833967e66d01f8ba8b7cc4b4137ed433c582a7
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-10-07 11:32:42 +0800

    updating sandbox

--- client/sandbox.h
+++ client/sandbox.h
@@ -2,22 +2,24 @@
 #include <stdio.h>
 
 typedef struct {
-    void  *addr;
-    size_t len; // the size (in bytes) of the region that was mmap'ed
+  void *addr;
+  size_t len;  // the size (in bytes) of the region that was mmap'ed
 } mapped_region_t;
 
 typedef struct {
-    void    *addr;    // absolute address
-    uint8_t  old_val; // expected value
-    uint8_t  new_val; // actual value
+  void *addr;       // absolute address
+  uint8_t old_val;  // expected value
+  uint8_t new_val;  // actual value
 } memdiff_t;
 
 extern const char *reg_names[32];
 
 void setup_signal_handlers();
+void restore_signal_handlers();
 void initialise();
 uint8_t *allocate_executable_buffer();
 void free_executable_buffer(uint8_t *sandbox);
 void prepare_sandbox(uint8_t *sandbox_ptr);
-void inject_instructions(uint8_t *sandbox_ptr, const uint32_t *instrs, size_t num_instrs);
+void inject_instructions(uint8_t *sandbox_ptr, const uint32_t *instrs,
+                         size_t num_instrs);
 void unmap_vdso_vvar();
